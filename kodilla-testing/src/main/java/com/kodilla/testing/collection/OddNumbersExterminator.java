d155220d213c4e2878167cc4c2f2bc4b6147b906
anna.Pelczarska <anna.pelczrska@gmail.com>
2018-07-22 21:27:44 +0200

    unit 6 corrections v1

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -7,8 +7,8 @@ public class OddNumbersExterminator {
     // List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
     //List<Integer> evenNumbers = new ArrayList<>();
 
-    public ArrayList exterminate(ArrayList<Integer> numbers) {
-        ArrayList<Integer> evenNumbers = new ArrayList<>();
+    public List exterminate(ArrayList<Integer> numbers) {
+        List<Integer> evenNumbers = new ArrayList<>();
 
         for (int number : numbers) {
             if (number % 2 == 0) {
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -17,10 +17,10 @@ public class CollectionTestSuite {
     @Test
      public void testOddNumbersExterminatorEmptyList(){
         //Given
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList());
+        List<Integer> numbers = new ArrayList<>(Arrays.asList());
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
         //When
-        List<Integer> result = exterminator.exterminate(numbers);
+        List<Integer> result = exterminator.exterminate((ArrayList<Integer>) numbers);
         System.out.println("Testing " + result);
         //Then
         Assert.assertEquals(result, Arrays.asList());
@@ -28,10 +28,10 @@ public class CollectionTestSuite {
     @Test
      public void testOddNumbersExterminatorNormalList(){
         //Given
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
         //When
-        ArrayList<Integer> result = exterminator.exterminate(numbers);
+        List<Integer> result = exterminator.exterminate((ArrayList<Integer>) numbers);
         System.out.println("Testing " + result);
         //Then
         Assert.assertEquals(result, Arrays.asList(2,4,6,8,10,12));
